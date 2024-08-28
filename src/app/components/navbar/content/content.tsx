4168f58b655d027167e75b2a725ae76d79b5c811
Azan80 <azanabid8888@gmail.com>
2024-08-28 18:00:50 +0500

    responsive card

--- src/app/components/navbar/content/content.tsx
+++ src/app/components/navbar/content/content.tsx
@@ -99,6 +99,14 @@ const Content = () => {
       book: 'img/icon/image (5).png'
     },
   ];
+  const index = 0;
+  const authImg = courses[index] ? courses[index].authimg : '';
+  const Price = courses[index] ? courses[index].price :''
+  const Time = courses[index] ? courses[index].duration :''
+  const Book= courses[index] ? courses[index].book:''
+const Lecture = courses[index] ? courses[index].lectures:''
+const Clock = courses[index] ? courses[index].clock:""
+
 
   const toggleCourses = () => {
     setVisibleCourses(showAll ? 3 : courses.length);
@@ -123,7 +131,7 @@ const Content = () => {
       <div className={styles.discription}>
         <p>
           We believe the world is more beautiful as each person gets skills <br />
-          and knows how to implement them. We don't give our students <br />
+          and knows how to implement them. We dont give our students <br />
           only lectures but professional training as well.
         </p>
       </div>
@@ -134,6 +142,7 @@ const Content = () => {
           width={100}
           height={40}
           className={styles.logo}
+
         />
       </div>
       <div className={styles.floater2}>
@@ -250,7 +259,7 @@ const Content = () => {
                   <span className={styles.lectures}>{course.lectures} Lecture</span>
                 </div>
                 <div className={styles.footer}>
-                  <span className={styles.price}>${course.price}.00</span>
+                  <span className={styles.price}>{course.price}.00</span>
                   <div className={styles.rating}>
                     {'★'.repeat(course.rating) + '☆'.repeat(5 - course.rating)}
                   </div>
@@ -283,6 +292,86 @@ const Content = () => {
   </div>
      
         
+      </div>
+      <div className={styles.container3}>
+        <div  className={styles.heading}>
+      <p>Browse {'>'} Photography {'>'} Education</p>
+        </div>
+        <h1 className={styles.subHead}>The Art of Seeing Photography<br></br>
+         Training for the Artist in You</h1>
+         <p className={styles.ContainerDescription}> 
+         Learn Practical Skills to Teach Online. Master the foundations<br></br>
+for creating engaging and effective online learning<br></br>
+and teaching
+         </p>
+        <div className={styles.Star}>
+          <p> ⭐⭐⭐⭐<a style={{
+color: 'white',
+          }}>☆</a></p>
+        </div>
+        <button className={styles.PhotoGraphy}>Photography</button>
+    <div className={styles.authImage}>
+    {authImg ? (
+        <img src={authImg} alt="Author Image" style={{ width: '40px', height: 'auto' }} />
+      ) : (
+        <p>No image available</p>
+      )}<a style={{
+        color: 'white',
+        marginTop:"1%",
+        marginLeft:"2%"      }}>by Jonathan Bakes</a>
+    </div>
+    <div className={styles.List}>
+      <div className={styles.detailItem}>
+        {Price ? (
+          <p className={styles.price}>${Price}.00</p>
+        ) : (
+          <p>No price available</p>
+        )}
+      </div>
+
+
+      <div className={styles.detailItem}>
+
+      {Clock ? (
+          <img className={styles.clock} src={Clock}  />
+        ) : (
+          <p>No time information available</p>
+        )}
+
+
+        {Time ? (
+          <p className={styles.time}>{Time}</p>
+        ) : (
+          <p>No time information available</p>
+        )}
+      </div>
+
+      <div className={styles.detailItem}>
+        {Book ? (
+          <img className={styles.bookImage} src={Book} alt="Book" />
+        ) : (
+          <p>No book image available</p>
+        )}
+
+
+  
+        {Lecture ? (
+          <p className={styles.lectureCount}>
+            {Lecture} Lectures
+          </p>
+        ) : (
+          <p>No lecture information available</p>
+        )}
+      </div>
+    </div>
+      <button className={styles.EnrollBtn}>
+        Enroll For Free
+      </button>
+      <div className={styles.Enroll}>
+      <p>Enroll to start your 7-day<br></br>
+full access free trial</p>
+      </div>
+    
       </div>
     </div>
   );
